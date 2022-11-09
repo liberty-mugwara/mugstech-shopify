024c05f147363f6c7a487f4944a6bc85dc1e8069
Liberty Mugwara <libertymugwara@live.com>
2022-11-09 11:40:01 +0200

    FEAT: Add metafields functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "./utils";
 export * from "./products";
 export * from "./orders";
+export * from "./bulk-operations";
+export * from "./meta-fields";
---
+++ src/meta-fields.ts
@@ -0,0 +1,27 @@
+import { bulkUpdate } from "./bulk-operations";
+import { gql } from "./api";
+
+export const setMetaFieldsMutation = gql`
+  mutation metafieldsSet($metafields: [MetafieldsSetInput!]!) {
+    metafieldsSet(metafields: $metafields) {
+      metafields {
+        key
+        namespace
+        value
+        createdAt
+        updatedAt
+      }
+      userErrors {
+        field
+        message
+        code
+      }
+    }
+  }
+`;
+
+export async function bulkSetMetafields(
+  JSONLReadStream: NodeJS.ReadableStream
+) {
+  return bulkUpdate({ JSONLReadStream, mutation: setMetaFieldsMutation });
+}
