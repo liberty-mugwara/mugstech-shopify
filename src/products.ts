d6f9d7a7c9879902e1d22ef8dae5aca4ec539d72
Liberty Mugwara <libertymugwara@live.com>
2022-12-08 08:05:35 +0200

    FEAT: Add product functions

--- src/products.ts
+++ src/products.ts
@@ -1,8 +1,9 @@
 import { gql, graphqlClient } from "./api";
 
-import { bulkUpdate } from "./bulk-operations";
+import { bulkQueryComplete, bulkUpdate } from "./bulk-operations";
 import { setTimeout } from "timers/promises";
 import { IProductCreateInput } from "./types";
+import { pipeline } from "node:stream/promises";
 
 export const updateProductTagsMutation = gql`
   mutation productUpdate($input: ProductInput!) {
@@ -127,4 +128,42 @@ export async function createProduct(input: IProductCreateInput) {
   `;
 
   return await graphqlClient.request(mutation, input);
+}
+
+export async function productExists(sku: string) {
+  const query = gql`
+  {
+    products(first:1,query:"sku:${sku}") {
+      nodes {
+        id
+      }
+    }
+  }
+  `;
+  const res = await graphqlClient.request(query);
+  if (res.products.nodes?.length) {
+    return { exists: true, productId: res.products.nodes[0].id };
+  }
+
+  return false;
+}
+
+export async function getAllProductIds(writeStream: NodeJS.WriteStream) {
+  const query = `
+  query {
+    products {
+      edges {
+        node {
+          id
+          variants(first:20){
+            id
+            sku
+          }
+        }
+      }
+    }
+  }
+  `;
+  const readStream = await bulkQueryComplete({ query });
+  await pipeline(readStream, writeStream);
 }
